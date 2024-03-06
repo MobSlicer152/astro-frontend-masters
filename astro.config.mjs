889a07525055dab279a73b702236c3a32cf8c84b
MobSlicer152 <brambleclaw1414@gmail.com>
2024-03-06 11:50:45 -0500

    Add RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mobslicer152.github.io/astro-frontend-masters/',
   integrations: [tailwind(), react(), solidJs()]
 });
